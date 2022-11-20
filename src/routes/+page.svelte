f782e4a5e37452945e4e4b115c1264d851da3fd5
Esben Kran <esben@kran.ai>
2022-11-20 21:25:43 +0000

    Ryebread

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,5 @@
 <svelte:head>
-  <title>Ryebread {"<3"}</title>
+  <title>ryebread manifesto</title>
   <meta
     name="description"
     content="Ryebread is a type of bread made with rye flour. It is usually denser and darker than bread made with wheat flour, and has a distinctive, slightly sour taste. Ryebread is popular in many European countries, particularly in Scandinavia and Germany."
@@ -7,30 +7,90 @@
 </svelte:head>
 
 <section>
-  <h1>Ryebread</h1>
-  <span class="welcome">
-    <picture>
-      <img
-        src="https://madensverden.dk/wp-content/uploads/2021/08/hjemmebagt-kernerugbroed.jpg"
-        alt="Welcome"
-      />
-    </picture>
-  </span>
-  <p>Ryebread is very good.</p>
+  <picture>
+    <img
+      src="https://madensverden.dk/wp-content/uploads/2021/08/hjemmebagt-kernerugbroed.jpg"
+      alt="Ryebread"
+    />
+  </picture>
+  <h1>the ryebread manifesto</h1>
+  <ul>
+    <li>
+      Ryebread is high in fiber, which can help to regulate digestion and keep
+      you feeling full longer.
+    </li>
+    <li>
+      Ryebread is a good source of whole grains, which provide essential
+      nutrients like vitamins, minerals, and antioxidants.
+    </li>
+    <li>
+      Ryebread has a lower glycemic index than white bread, meaning it won't
+      cause spikes in blood sugar levels.
+    </li>
+    <li>
+      Ryebread is naturally low in fat and calories, making it a healthy choice
+      for those watching their weight.
+    </li>
+    <li>
+      Ryebread is a good option for people with celiac disease or gluten
+      intolerance, as it is naturally gluten-free.
+    </li>
+  </ul>
+  <h2>ryebread is the best bread in the world</h2>
+  <p>
+    Ryebread is a type of bread made with rye flour. It is usually denser and
+    darker than bread made with wheat flour, and has a distinctive, slightly
+    sour taste. Ryebread is popular in many European countries, particularly in
+    Scandinavia and Germany. Ryebread philosophically represents all that is
+    good about bread. It is simple, honest, and wholesome. It is the bread of
+    the people, and it nourishes both body and soul. Compared to white bread,
+    ryebread is a nutritional powerhouse, packed with fiber, vitamins, minerals,
+    and antioxidants. It is also lower on the glycemic index, meaning it won't
+    cause spikes in blood sugar levels. For all these reasons, ryebread is the
+    best bread in the world.
+  </p>
+  <h2>history</h2>
+  <p>
+    Ryebread has been around for centuries. It was originally developed in
+    Europe as a way to use rye flour, which was a cheaper and more readily
+    available alternative to wheat flour. Over time, rye bread became a staple
+    food in many parts of Europe, particularly in Scandinavia and Germany. It
+    has always represented the strength and fortitude of the human spirit, as it
+    is a bread that can withstand the harshest of conditions. In recent years,
+    ryebread has experienced a resurgence in popularity, as more and more people
+    are realizing its nutritional benefits. Today, ryebread is enjoyed by people
+    all over the world, and it is sure to remain a staple food for generations
+    to come.
+  </p>
+  <div class="line" />
 </section>
 
 <style>
-  section {
-    display: flex;
-    flex-direction: column;
-    align-items: left;
-    justify-content: center;
-    max-width: 900px;
-    margin: 0 auto;
+  :global(body) {
+    font-family: monospace;
   }
+
+  p,
+  li {
+    font-size: 1rem;
+    line-height: 1.5;
+  }
+
+  h2 {
+    font-size: 1.25rem;
+    margin-top: 2rem;
+  }
+
   h1 {
-    font-size: 2.5rem;
+    font-size: 2rem;
     text-align: left;
+  }
+
+  .line {
+    width: 100%;
+    height: 1px;
+    background-color: #000;
+    margin: 1rem 0;
   }
 
   img {
