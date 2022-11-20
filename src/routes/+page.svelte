69ac7d68b80ef18ef9b55139b9daf9a32f650600
Esben Kran <esben@kran.ai>
2022-11-20 21:14:55 +0000

    Ryebread

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,22 +1,3 @@
-<script>
-  import { onMount } from "svelte";
-
-  let images = [
-    "https://madensverden.dk/wp-content/uploads/2021/08/hjemmebagt-kernerugbroed.jpg",
-  ];
-
-  //   Set a random initial poem
-  let poem = poems[Math.floor(Math.random() * poems.length)];
-  let image = images[Math.floor(Math.random() * images.length)];
-
-  onMount(() => {
-    setInterval(() => {
-      poem = poems[Math.floor(Math.random() * poems.length)];
-      image = images[Math.floor(Math.random() * images.length)];
-    }, 10000);
-  });
-</script>
-
 <svelte:head>
   <title>Ryebread {"<3"}</title>
   <meta
@@ -29,7 +10,10 @@
   <h1>Ryebread</h1>
   <span class="welcome">
     <picture>
-      <img src={image} alt="Welcome" />
+      <img
+        src="https://madensverden.dk/wp-content/uploads/2021/08/hjemmebagt-kernerugbroed.jpg"
+        alt="Welcome"
+      />
     </picture>
   </span>
   <p>Ryebread is very good.</p>
@@ -39,11 +23,16 @@
   section {
     display: flex;
     flex-direction: column;
-    align-items: center;
+    align-items: left;
     justify-content: center;
     max-width: 900px;
     margin: 0 auto;
   }
+  h1 {
+    font-size: 2.5rem;
+    text-align: left;
+  }
+
   img {
     max-width: 400px;
   }
